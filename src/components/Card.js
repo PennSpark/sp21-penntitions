7e54424afd146b01cef158ba21b4fb7fd98378cd
janna-goliff <gojanna@sas.upenn.edu>
2021-05-04 02:57:35 -0400

    integrated Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
 const Card = (props) => {
-    return <div className='w-2/6 shadow-md rounded-lg border border-grey-400 outline-none '>
+    return <div className='w-2/3 shadow-md rounded-lg border border-grey-400 outline-none '>
         <div className='px-4 py-3 border-b'>
             <div className='flex justify-between'>
-                <h1 className='font-bold text-3xl'>Title</h1>
-                <h1 className='bg-blue-700 text-white text-base rounded-full px-3 py-1'>Active</h1>
+                <h1 className='font-bold text-3xl'>{props.title ? props.title : "Loading Title"}</h1>
+                <h1 className='bg-blue-700 text-white text-base px-3 py-1 rounded-full md:rounded-md sm:rounded-sm'>{props.isActive ? "Active" : "Inactive"}</h1>
             </div>
             <div className='flex justify-between'>
-                <h2>From</h2>
-                <h2>Supporters</h2>
+                <h2>By {props.author ? props.author : "Loading Author"}, {props.dateCreated ? props.dateCreated : "Loading Date"}</h2>
+                <h2>{props.supporters ? props.supporters : "0"} Supporters</h2>
             </div>
         </div>
-        <div className='px-4 py-2 font-medium'>Description <a className='font-bold text-blue-700 underline'>Link</a></div>
-        <div className='px-4 py-2'>Tags</div>
+        <div className='px-4 py-2 font-medium'>{props.description ? props.description : "Loading description"} <a href={"/petition/" + props.petitionId} className='font-bold text-blue-700 underline'>See more</a></div>
+        <div className='px-4 py-2 text-gray-500'>Tags</div>
     </div>;
   };
   export default Card;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useAuth } from '../contexts/AuthContext';
 import { useState, useEffect } from 'react';
 import { db } from '../firebase';
+import Card from '../components/Card';
 
 import UserNavbar from '../components/UserNavbar';
 
@@ -13,9 +14,25 @@ const Home = (props) => {
 
   // }, []);
 
-  return <div>
-    <UserNavbar />
-    Home {userData ? userData.firstName : "loading"}
-    </div>;
+  return (<div className="w-screen h-screen">
+      <UserNavbar />
+      <div className="flex w-full">
+        <div className='w-1/3 bg-blue-100 h-screen'>filler</div>
+        <div className='w-screen pl-8 pt-4'>
+          <h1 className="text-3xl font-bold mb-4">Latest</h1>
+          <Card 
+          className="m-0"
+            title="Petition title ex"
+            isActive="true"
+            author="filler author"
+            dateCreated="date"
+            numSupporters="100"
+            description="this is an example description of the petition. put something compelling here!"
+            tags=""
+            linkedPetitionId="abcd"
+          />
+        </div>
+      </div>
+    </div>);
 };
 export default Home;
