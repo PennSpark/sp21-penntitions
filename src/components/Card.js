8eba2f9e1cc2aebfd0fea99d9ffd3882f8ce9910
janna-goliff <gojanna@sas.upenn.edu>
2021-05-06 01:44:07 -0400

    minor style:

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ const Card = (props) => {
         <div className='px-4 py-3 border-b'>
             <div className='flex justify-between'>
                 <h1 className='font-bold text-3xl'>{props.title ? props.title : "Loading Title"}</h1>
-                <h1 className='bg-blue-700 text-white text-base px-3 py-1 rounded-full md:rounded-md sm:rounded-sm'>{props.isActive ? "Active" : "Inactive"}</h1>
+                <h1 className='bg-blue-700 text-white text-base px-6 py-1 rounded-full lg:rounded-full md:rounded-md sm:rounded-sm'>{props.isActive ? "Active" : "Inactive"}</h1>
             </div>
             <div className='flex justify-between'>
                 <h2>by {props.author ? props.author : "Loading Author"}, {props.dateCreated ? props.dateCreated : "Loading Date"}</h2>
